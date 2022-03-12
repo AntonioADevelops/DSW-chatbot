64fb86b76df659fe2926686aa6294056a4e2b83f
AntonioADevelops <ayalarantonio16@gmail.com>
2022-03-12 00:32:55 -0800

    changes

--- webapp.py
+++ webapp.py
@@ -1,3 +1,4 @@
+from turtle import update
 from urllib import response
 from flask import Flask, url_for, render_template, request, Markup
 import markupsafe 
@@ -5,11 +6,12 @@ import random
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-greetings = ['hi', 'Hi', 'hello', 'Hello', 'greetings', 'Greetings', 'howdy', 'Howdy', 'hey', 'Hey']
+greetings = ['Hi', 'Hello', 'Greetings', 'Howdy', 'Hey', 'Sup', 'Whats Good', 'Wassup',]
 
 bot_greetings = ['Hey! I\'m HelloBot.', 'Greetings User! My Name\'s HelloBot.', 'Howdy Partner! I go by HelloBot.']
 
-inputs = []
+confused = ['Sorry, I don\'t quite understand.', 'I didnt quite catch that. Please rephrase your greeting.', 'I haven\'t learned that greeting, please ask in a different manner.']
+
 
 @app.route("/")
 def render_main():
@@ -19,18 +21,17 @@ def render_main():
 
 def user_input():
     u_input = request.args['response']
-    
-    if u_input in greetings:
+    chat_update = request.args["storedData"]
+        
+    if u_input.upper() in (name.upper() for name in greetings):
         reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(bot_greetings) + Markup('</p><br>')
-        return render_template('home.html', response = reply)
+        update = Markup(chat_update)
+        return render_template('home.html', response = update + reply)
     
-    else:
-        reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">Sorry, I don\'t quite understand. Greet me in in a different manner.</p><br>')
-        return render_template('home.html', response = reply)
-    
-def update():
-    update_text = request.args.get['storedData']
-    return render_template('home.html', response = update_text)
+    elif u_input not in greetings:
+        reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(confused) + Markup('</p><br>')
+        update = Markup(chat_update)
+        return render_template('home.html', response = update + reply)
 
 if __name__=="__main__":
     app.run(debug=False)
