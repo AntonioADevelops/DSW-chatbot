4adbb147704b942736107ce49f7d3ebcb95567e6
AntonioADevelops <ayalarantonio16@gmail.com>
2022-03-14 09:57:01 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -10,7 +10,19 @@ greetings = ['Hi', 'Hello', 'Greetings', 'Howdy', 'Hey', 'Sup', 'Whats Good', 'W
 
 bot_greetings = ['Hey! I\'m HelloBot.', 'Greetings User! My Name\'s HelloBot.', 'Howdy Partner! I go by HelloBot.']
 
-confused = ['Sorry, I don\'t quite understand. Say hi a different way.', 'I didnt quite catch that. Please rephrase your greeting.', 'I haven\'t learned that greeting, please ask in a different manner.']
+confused = ['Sorry, I don\'t quite understand. Say it in a different way.', 'I didnt quite catch that. Please rephrase your sentence.', 'I haven\'t learned what that means, please ask in a different manner.']
+
+u_how_are_you = ['How are you?', 'How are you?', 'How do you do', 'How do you do?', 'How do you feel today?', 'How do you feel today?']
+ 
+b_how_are_you = ['I\'m doing pretty good.']
+
+u_origin_who = ['Who made you', 'Who made you?', 'Who created you', 'Who created you?']
+
+b_origin_who = ['I was developed by Antonio A. for a school project.']
+
+u_origin_how = ['How were you created', 'How were you created?', 'How were you made', 'How were you made?']
+
+b_origin_how = ['I was born from code! My creator spent countless hours copying and pasting code from stackoverflow until I worked.']
 
 
 @app.route("/")
@@ -22,15 +34,26 @@ def render_main():
 def user_input():
     u_input = request.args['response']
     chat_update = request.args["storedData"]
+    update = Markup(chat_update)
         
     if u_input.upper() in (name.upper() for name in greetings):
         reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(bot_greetings) + Markup('</p><br>')
-        update = Markup(chat_update)
+        return render_template('home.html', response = update + reply)
+    
+    elif u_input.upper() in (name.upper() for name in u_how_are_you):
+        reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(b_how_are_you) + Markup('</p><br>')
+        return render_template('home.html', response = update + reply)
+    
+    elif u_input.upper() in (name.upper() for name in u_origin_who):
+        reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(b_origin_who) + Markup('</p><br>')
+        return render_template('home.html', response = update + reply)
+    
+    elif u_input.upper() in (name.upper() for name in u_origin_how):
+        reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(b_origin_how) + Markup('</p><br>')
         return render_template('home.html', response = update + reply)
     
     elif u_input not in greetings:
         reply = Markup('<p class="user">') + u_input + Markup('</p><br><p class="bot">') + random.choice(confused) + Markup('</p><br>')
-        update = Markup(chat_update)
         return render_template('home.html', response = update + reply)
 
 if __name__=="__main__":
