b398b61589a183d7a006ad6ce74d3683f60dbde3
AntonioADevelops <ayalarantonio16@gmail.com>
2022-03-15 09:03:37 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -1,7 +1,4 @@
-from turtle import update
-from urllib import response
 from flask import Flask, url_for, render_template, request, Markup
-import markupsafe 
 import random
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
